9016ba17d490865a5bd9784f6e0a819d3dcb4ed2
Kudret <kudret.kustul.pro@gmail.com>
2018-01-31 18:39:39 +0100

    dz

--- src/components/forms/SearchBookForm.js
+++ src/components/forms/SearchBookForm.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { Form, Dropdown } from "semantic-ui-react";
 import axios from "axios";
+import PropTypes from "prop-types";
 
 class SearchBookForm extends React.Component {
 	state = {
 		query: "",
 		loading: false,
-		success: false,
 		options: [],
 		books: {}
+	};
+	onChange = (e, data) => {
+		this.setState({ query: data.value });
+		this.props.onBookSelect(this.state.books[data.value]);
 	};
 	onSearchChange = (e, data) => {
 		clearTimeout(this.timer);
@@ -21,8 +25,21 @@ class SearchBookForm extends React.Component {
 		if (!this.state.query) return;
 		this.setState({ loading: true });
 		axios
-			.get(`/api/books/search?q=${this.setState.query}`)
-			.then(res => res.data.books);
+			.get(`/api/books/search?q=${this.state.query}`)
+			.then(res => res.data.books)
+			.then(books => {
+				const options = [];
+				const booksHash = {};
+				books.forEach(book => {
+					booksHash[book.goodreadsId] = book;
+					options.push({
+						key: book.goodreadsId,
+						value: book.goodreadsId,
+						text: book.title
+					});
+				});
+				this.setState({ loading: false, options, books: booksHash });
+			});
 	};
 	render() {
 		return (
@@ -35,9 +52,13 @@ class SearchBookForm extends React.Component {
 					onSearchChange={this.onSearchChange}
 					options={this.state.options}
 					loading={this.state.loading}
+					onChange={this.onChange}
 				/>
 			</Form>
 		);
 	}
 }
+SearchBookForm.propTypes = {
+	onBookSelect: PropTypes.func.isRequired
+};
 export default SearchBookForm;
